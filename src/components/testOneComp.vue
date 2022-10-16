e4bdc0d75daf2a4a20afc9bfb79ee10c566cc935
Kim JaeHyun <69341843+jh8057@users.noreply.github.com>
2022-10-16 20:25:39 +0900

    add end tag

--- src/components/testOneComp.vue
+++ src/components/testOneComp.vue
@@ -25,7 +25,7 @@
     <article v-show="end" class="QuestionEnd">
       <div>
         창을 종료하지 말고 지시가 있기 전까지<br />
-        <span class="redfont">대기하여 주시기 바랍니다.<span><br />
+        <span class="redfont">대기하여 주시기 바랍니다.</span><br />
       </div>
       <div style="margin: 30px">
         <button @click="goTestTwo" style="font-size: 15px; padding: 10px">
