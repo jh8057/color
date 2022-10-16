a715b571fd2285b7e144f81a95a8faac39491109
Kim JaeHyun <69341843+jh8057@users.noreply.github.com>
2022-10-16 20:40:44 +0900

    change code
    - confirm
    - 문구 추가

--- src/components/testOneComp.vue
+++ src/components/testOneComp.vue
@@ -101,7 +101,9 @@ export default defineComponent({
     };
 
     const goTestTwo = () => {
-      store.commit("setStep", 5);
+      if(window.confirm('지시에 따라 다음단계로 넘어가주세요.\n 다음 단계로 넘어갈까요?')){
+        store.commit("setStep", 5);
+      }
     };
 
     onMounted(() => {
